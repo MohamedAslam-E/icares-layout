660ecab2a898f99b1a03f68c2fed5f4dd7ba0278
Mohamed Aslam <aslamnjr007@mail.comgit config --global user,email aslamnjr007@gamil.com>
2024-01-21 12:12:20 +0530

    dd

---
+++ footer.php
@@ -0,0 +1,104 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-12 mb-4">
+                <img src="images/logo.png" class="logo img-fluid" alt="">
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-12 mb-4">
+                <h5 class="site-footer-title mb-3">Quick Links</h5>
+
+                <ul class="footer-menu">
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Our Story</a></li>
+
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Newsroom</a></li>
+
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Causes</a></li>
+
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Become a volunteer</a></li>
+
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Partner with us</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-12 mx-auto">
+                <h5 class="site-footer-title mb-3">Contact Infomation</h5>
+
+                <p class="text-white d-flex mb-2">
+                    <i class="bi-telephone me-2"></i>
+
+                    <a href="tel: 305-240-9671" class="site-footer-link">
+                        305-240-9671
+                    </a>
+                </p>
+
+                <p class="text-white d-flex">
+                    <i class="bi-envelope me-2"></i>
+
+                    <a href="mailto:info@yourgmail.com" class="site-footer-link">
+                        donate@charity.org
+                    </a>
+                </p>
+
+                <p class="text-white d-flex mt-3">
+                    <i class="bi-geo-alt me-2"></i>
+                    Akershusstranda 20, 0150 Oslo, Norway
+                </p>
+
+                <a href="#" class="custom-btn btn mt-3">Get Direction</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-footer-bottom">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-md-7 col-12">
+                    <p class="copyright-text mb-0">Copyright © 2036 <a href="#">Kind Heart</a> Charity Org.
+                        Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a><br>Distribution:
+                        <a href="https://themewagon.com">ThemeWagon</a>
+                    </p>
+                </div>
+
+                <div class="col-lg-6 col-md-5 col-12 d-flex justify-content-center align-items-center mx-auto">
+                    <ul class="social-icon">
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-twitter"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-facebook"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-instagram"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-linkedin"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="https://youtube.com/templatemo" class="social-icon-link bi-youtube"></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- JAVASCRIPT FILES -->
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.sticky.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/click-scroll.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/counter.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/custom.js"></script>
+
+<?php wp_footer();?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/jpeg" href="/images/icons/icares.jpeg">
+    <title>Kind Heart Charity - Free Bootstrap 5.2.2 CSS Template</title>
+
+    <!-- CSS FILES -->
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/css/templatemo-kind-heart-charity.css" rel="stylesheet">
+    <!--
+
+TemplateMo 581 Kind Heart Charity
+
+https://templatemo.com/tm-581-kind-heart-charity
+
+-->
+<?php wp_head();?>
+</head>
+
+<body id="section_1">
+    <header class="site-header">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-8 col-12 d-flex flex-wrap">
+                    <p class="d-flex me-4 mb-0">
+                        <i class="bi-geo-alt me-2"></i>
+                        Akershusstranda 20, 0150 Oslo, Norway
+                    </p>
+
+                    <p class="d-flex mb-0">
+                        <i class="bi-envelope me-2"></i>
+
+                        <a href="mailto:info@company.com">
+                            info@company.com
+                        </a>
+                    </p>
+                </div>
+
+                <div class="col-lg-3 col-12 ms-auto d-lg-block d-none">
+                    <ul class="social-icon">
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-twitter"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-facebook"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-instagram"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-youtube"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="#" class="social-icon-link bi-whatsapp"></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </header>
